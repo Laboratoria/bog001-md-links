c1c0a2ec59da06d52c7d08fbf1827523d0706947
AnaRativa09 <anarativa09@gmail.com>
2020-09-21 15:20:55 -0500

    Change branch

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
-};
+}; */
+
+console.log('Holi, me llamo Ana');
+
+const userPath = 'aquí la ruta absoluta';
+
+const mdLinks= (path, options) => {
+
+}
+
+const absolutePath = (path) => {
+  console.log(path.isAbsolute(path));
+  //condición de si es verdadera o no
+}
+
+absolutePath(userPath)
