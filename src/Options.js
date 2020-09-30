419bb99927a17cb51b51fd745a0b9f9d9aee20f3
daniela ramirez <daniedi.solution@gmail.com>
2020-09-30 18:49:21 -0500

    funciones y test

--- src/Options.js
+++ src/Options.js
@@ -76,39 +76,4 @@ module.exports = {
   statsAndValidate,
 };
 
-// const cli = (PATH, opcion, opcion2) => {
-//   if (PATH === undefined) {
-//     return new Promise((resolve) =>
-//       resolve("Ingrese una ruta relativa o absoluta")
-//     );
-//   }
-//   leading.mdLinks(PATH, { validate: false }).then((links) => {
-//     if (links.length === 0) {
-//       // console.log("El archivo que ingresaste no contiene links");
-//     }
-//   });
-//   if ((opcion === "--stats" && opcion2 === "--validate") ||(opcion === "--validate" && opcion2 === "--stats")) {
-//     return leading
-//       .mdLinks(PATH, { validate: true })
-//       .then((resp) => statsAndValidate(resp))
-//       .then((links) => {
-//         const result = `Total: ${links.Total}\nUnique: ${links.Unique}\nBroken: ${links.Broken}`;
-//         return result;
-//       });
-//   }
-//   if (opcion === "--validate") {
-//     return leading.mdLinks(PATH, { validate: true }).then((res) => res);
-//   }
-
-//   if (opcion === "--stats") {
-//     return leading
-//       .mdLinks(PATH, { validate: true })
-//       .then((resp) => stats(resp))
-//       .then((links) => {
-//         const result = `Total: ${links.Total}\nUnique: ${links.Unique}`;
-//         return result;
-//       });
-//   } else if (opcion === undefined) {
-//     return leading.mdLinks(PATH, { validate: false }).then((res) => res);
-//   }
-// };
+ 
