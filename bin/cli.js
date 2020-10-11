c0bef40c5e1adb84bf4fcb70e1283b28643be623
AnaRativa09 <anarativa09@gmail.com>
2020-10-10 21:17:30 -0500

    Delete comments

--- bin/cli.js
+++ bin/cli.js
@@ -7,19 +7,12 @@ const program = require('commander');
 const mdLinks = require('../src/index.js');
 const { getStats, getStatsValidate } = require('../src/functions/stats.js');
 
-// const pathPrueba = '/Users/Ana/Desktop/bog001-md-links/test/docs/docs-2/docs-3';
-
 CFonts.say('MD-LINKS!', {
   font: 'block',
   align: 'left',
   colors: ['#5dc1b9', '#ffdfa1'],
   env: 'node',
 });
-
-// eslint-disable-next-line max-len
-// console.log(chalk.hex('8BDCD6').underline('¡Hola mundo!'), chalk.hex('FDA0A5').italic('Amo esto <3'));
-// eslint-disable-next-line max-len
-// console.log(`¡Hola mundo! ${chalk.hex('8BDCD6').underline('Estoy probando')} y creo que ${chalk.hex('FDA0A5').italic('amo esto <3')}`);
 
 program
   .version('0.0.1')
@@ -51,7 +44,6 @@ if (!program.validate && program.stats) {
       console.log(chalk.hex('FB5961')('--------------------'));
       console.log(chalk.hex('8BDCD6')('       Stats'));
       console.log(chalk.hex('FB5961')('--------------------'));
-      /* console.log(`Total: ${statsLinks.Total}. Unique: ${statsLinks.Unique}.`); */
       console.table(statsLinks);
     })
     .catch(console.error);
@@ -65,10 +57,6 @@ if (program.validate && program.stats) {
       console.log(chalk.hex('FB5961')('--------------------'));
       console.log(chalk.hex('8BDCD6')('  Stats + Validate'));
       console.log(chalk.hex('FB5961')('--------------------'));
-      // console.log(`
-      // Total: ${statsLinksValidate.Total}.
-      // Unique: ${statsLinksValidate.Unique}.
-      // Broken: ${statsLinksValidate.Broken}`);
       console.table(statsLinksValidate);
     })
     .catch(console.error);
